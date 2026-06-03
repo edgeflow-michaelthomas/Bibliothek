8e803fa621470d3a53df61d057851c6435c83525
Michael Thomas <michit.berlin@googlemail.com>
2026-06-03 10:51:58 +0200

    Added inventory Id tracking in Bibliothek

--- src/main/java/de/schulung/bibliothek/administration/Bibliothek.java
+++ src/main/java/de/schulung/bibliothek/administration/Bibliothek.java
@@ -7,6 +7,7 @@ import java.util.List;
 
 public class Bibliothek {
     private final List<Medium> mediums =  new ArrayList<>();
+    private int currentMaxId = 0;
 
     public boolean addToStock(Medium medium){
         if (!mediums.contains(medium)){
@@ -18,5 +19,16 @@ public class Bibliothek {
 
     public boolean removeFromStock(Medium medium){
         return mediums.remove(medium);
+    }
+
+    public void printStock() {
+        for (Medium medium : mediums) {
+            System.out.println(medium);
+        }
+    }
+
+    public int getNextId() {
+        currentMaxId++;
+        return currentMaxId;
     }
 }
