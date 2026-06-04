5b83f5e187a16eae9ff22f9832ae651b2031998d
Michael Thomas <michit.berlin@googlemail.com>
2026-06-04 15:08:01 +0200

    Added Skeleton for Member Class and task list

--- src/main/java/de/schulung/bibliothek/administration/Member.java
+++ src/main/java/de/schulung/bibliothek/administration/Member.java
@@ -1,4 +1,32 @@
 package de.schulung.bibliothek.administration;
 
+import java.time.LocalDate;
+
 public class Member {
+    private String id;
+    private String firstName;
+    private String lastName;
+    private LocalDate birthDate;
+    private String email;
+
+    // Datumsformat: YYYY-MM-DD
+    public Member(String id, String firstName, String lastName, String birthDateString, String email) {
+        this.id = id;
+        this.firstName = firstName;
+        this.lastName = lastName;
+        this.birthDate = LocalDate.parse(birthDateString);
+        this.email = email;
+    }
 }
+
+
+/*
+Aufgaben:
+TODO: Überlegt, welche Form für die ID im Mitglied sinnvoll ist
+TODO: Implementiert die Member.java-Klasse zu Ende
+TODO: Testet das Erstellen von Mitgliedern
+TODO: Fügt Mitgliederverwaltung in die Bibliothek hinzu
+TODO: Erstellt einen Mitgliedergenerator
+
+TODO: Implementiert das Ausleihen von Medien
+*/
